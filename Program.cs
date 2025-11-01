4c9ddcdedc04c1a412690b5bd63493da6449ed45
Jessica <jpanndora@gmail.com>
2025-11-01 18:56:41 -0300

    Exercicio Concluido

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Dó, ré, mi, fá, fá, fá,
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("fá, "); Console.Beep(1759, 600);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 600);
+Console.Write("fá,"); Console.Beep(1759, 600);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, ré, dó, ré, ré, ré,
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Thread.Sleep(500);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("ré,"); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+//Dó, sol, fá, mi, mi, mi,
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("sol, "); Console.Beep(1980, 200);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Thread.Sleep(500);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+//Dó, ré, mi, fá, fá, fá,
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200); 
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("fá, "); Console.Beep(1759, 600);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 600);
+Console.Write("fá."); Console.Beep(1759, 600);
+Thread.Sleep(1000);
